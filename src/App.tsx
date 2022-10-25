22c66e0fba29221026f034f08aee1bec7a66fae7
Khalid Najmeddine <knajm574@gmail.com>
2022-10-25 09:57:04 -0600

    feat: Application rendering AppShell layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
+import { ColorSchemeProvider, MantineProvider, Paper, ColorScheme } from '@mantine/core'
+import { useHotkeys, useLocalStorage } from '@mantine/hooks'
 import './App.css'
+import AppShellLayout from './components/AppShell'
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: 'light',
+    getInitialValueInEffect: true,
+  });
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]]);
 
   return (
     <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{colorScheme}}>
+        <Paper>
+        <AppShellLayout />
+        </Paper>
+      </MantineProvider>
+      </ColorSchemeProvider>
     </div>
   )
 }
