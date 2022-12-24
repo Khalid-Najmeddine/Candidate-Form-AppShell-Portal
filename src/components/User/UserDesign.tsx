3fbd1622bf8b95fb3a4029092ba30bdf23e1b482
Khalid Najmeddine <knajm574@gmail.com>
2022-12-24 02:00:43 -0700

    feat: user design complete

---
+++ src/components/User/UserDesign.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { TbChevronRight, TbChevronLeft } from 'react-icons/tb'
+import { UnstyledButton, Group, Avatar, Text, Box, useMantineTheme } from '@mantine/core'
+
+export default function UserDesign() {
+  const theme = useMantineTheme()
+
+  return (
+    <Box
+      sx={{
+        paddingTop: theme.spacing.sm,
+        borderTop: `1px solid ${theme.colorScheme === 'dark' ? theme.colors.dark[4] : theme.colors.gray[2]}`,
+      }}>
+      <UnstyledButton
+        sx={{
+          display: 'block',
+          width: '100%',
+          padding: theme.spacing.xs,
+          borderRadius: theme.radius.sm,
+          color: theme.colorScheme === 'dark' ? theme.colors.dark[0] : theme.black,
+
+          '&:hover': {
+            backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[0],
+          },
+        }}>
+        <Group sx={{ display: 'flex', flexDirection: 'column', textAlign: 'center' }}>
+          <Avatar src="/images/Profile-Picture.jpeg" radius="xl" /> 
+          <Box sx={{ flex: 1 }}>
+            <Text size="sm" weight={500}>
+              Khalid Najmedidne
+            </Text>
+            <Text color="dimmed" size="xs">
+              khalid.najmeddine05@gmail.com
+            </Text>
+          </Box>
+          {theme.dir === 'ltr' ? <TbChevronRight size={18} /> : <TbChevronLeft size={18} />}
+        </Group>
+      </UnstyledButton>
+    </Box>
+  )
+}
