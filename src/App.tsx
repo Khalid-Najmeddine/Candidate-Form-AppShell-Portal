e95b9387aa3d2592677efcf9bec33bed7f17ce2e
Khalid Najmeddine <knajm574@gmail.com>
2022-12-24 02:09:30 -0700

    feat: Running version of the Candidate Form App

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,38 @@
-import { ColorSchemeProvider, MantineProvider, Paper, ColorScheme } from '@mantine/core'
-import { useHotkeys, useLocalStorage } from '@mantine/hooks'
-import './App.css'
-import AppShellLayout from './components/AppShell'
+import {ColorSchemeProvider,MantineProvider,Paper,ColorScheme,} from "@mantine/core";
+import { useHotkeys, useLocalStorage } from "@mantine/hooks";
+import "./App.css";
+import "./index.css";
+import AppShellDesign from "./components/AppShell/AppShellDesign";
+import "./styles.css";
+import React from "react";
 
 
-function App() {
+export default function App() {
   const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
-    key: 'mantine-color-scheme',
-    defaultValue: 'light',
+    key: "mantine-color-scheme",
+    defaultValue: "light",
     getInitialValueInEffect: true,
   });
 
   const toggleColorScheme = (value?: ColorScheme) =>
-    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
+    setColorScheme(value || (colorScheme === "dark" ? "light" : "dark"));
 
-  useHotkeys([['mod+J', () => toggleColorScheme()]]);
+  useHotkeys([["mod+J", () => toggleColorScheme()]]);
 
   return (
-    <div className="App">
-      <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-      <MantineProvider theme={{colorScheme}}>
-      <Paper>
-      <AppShellLayout />
-      </Paper>
-      </MantineProvider>
-      </ColorSchemeProvider>
-    </div>
-  )
+    <>
+      <div className="App">
+        <ColorSchemeProvider
+          colorScheme={colorScheme}
+          toggleColorScheme={toggleColorScheme}
+        >
+          <MantineProvider theme={{ colorScheme }}>
+            <Paper>
+              <AppShellDesign />
+            </Paper>
+          </MantineProvider>
+        </ColorSchemeProvider>
+      </div>
+    </>
+  );
 }
-
-export default App
